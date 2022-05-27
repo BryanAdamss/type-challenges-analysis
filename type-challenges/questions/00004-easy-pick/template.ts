bf0278229731479da2e9b63fef8e0e9885344c37
ghchu <ghchu@iflytek.com>
2022-05-27 22:53:39 +0800

    feat: 0004-easy-pick

--- type-challenges/questions/00004-easy-pick/template.ts
+++ type-challenges/questions/00004-easy-pick/template.ts
@@ -1,1 +1,40 @@
-type MyPick<T, K> = any
+//js
+function MyPick(todo, keys) {
+  const obj = {}
+
+  keys.forEach(key => {
+    if (key in todo) {
+      obj[key] = todo[key]
+    }
+  })
+
+  return obj
+}
+
+// 重点(js->TS)
+// 1. 返回一个对象-> type xxx = {}
+// 2. 遍历keys-> 遍历联合类型；[P in K]
+// 3. 取值 todo[key] ->索引类型；T[P]
+// 4. 检查todo中是否存在key-> 获取类型中所有key+泛型约束；K extends keyof T
+
+// ts
+type MyPick<T, K extends keyof T> = {
+  [P in K]: T[P]
+}
+
+// 知识点
+// 遍历联合类型
+// mapped
+// https://www.typescriptlang.org/docs/handbook/2/mapped-types.html
+
+// 索引类型
+// indexed
+// https://www.typescriptlang.org/docs/handbook/2/indexed-access-types.html
+
+// 获取类型中的所有key，类比Object.keys()
+// keyof、lookup
+// https://www.typescriptlang.org/docs/handbook/release-notes/typescript-2-1.html#keyof-and-lookup-types
+
+// 泛型约束
+// extends
+// https://www.typescriptlang.org/docs/handbook/2/generics.html#generic-constraints
