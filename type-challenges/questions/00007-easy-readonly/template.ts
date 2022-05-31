ee5d40d20abcee813593466100b09ddd161efc35
ghchu <ghchu@iflytek.com>
2022-05-31 21:01:44 +0800

    feat: 00007-easy-readonly

--- type-challenges/questions/00007-easy-readonly/template.ts
+++ type-challenges/questions/00007-easy-readonly/template.ts
@@ -1,1 +1,38 @@
-type MyReadonly<T> = any
+// js
+
+function MyReadonly(obj) {
+  let temp = null
+  for (const k in obj) {
+    if (obj.hasOwnProperty(k)) {
+      temp[`readonly ${k}`] = obj[k]
+    }
+  }
+
+  return temp
+}
+
+// 重点(js->TS)
+// 1. 返回一个对象-> type xxx ={}
+// 2. 遍历 obj所有属性 (js对象 ts接口)-> K in keyof T
+// 3. 添加 readonly关键字-> readonly xxxx
+// 4. 通过k获取obj(接口)中的值-> T[K]
+
+// ts
+type MyReadonly<T> = {
+  readonly [K in keyof T]: T[K]
+}
+
+// 获取interface所有key
+interface ToDo {
+  a: string
+  b: string
+}
+
+type ks = keyof ToDo // ks 'a' | 'b'
+
+const testKsTypeA: ks = 'a'
+const testKsTypeB: ks = 'b'
+const testKsTypeC: ks = 'c' // error
+
+// readonly
+// https://www.typescriptlang.org/docs/handbook/utility-types.html#readonlytype
