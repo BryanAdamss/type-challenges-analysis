3e8373bedb56a191d09a782ea233a45e2e160ee7
BryanAdamss <bryanadamss@foxmail.com>
2022-07-17 22:28:10 +0800

    feat: 00018-easy-tuple-length

--- type-challenges/questions/00004-easy-pick/test-cases.ts
+++ type-challenges/questions/00004-easy-pick/test-cases.ts
@@ -4,7 +4,7 @@ type cases = [
   Expect<Equal<Expected1, MyPick<Todo, 'title'>>>,
   Expect<Equal<Expected2, MyPick<Todo, 'title' | 'completed'>>>,
   // @ts-expect-error
-  MyPick<Todo, 'title' | 'completed' | 'invalid'>,
+  MyPick<Todo, 'title' | 'completed' | 'invalid'>
 ]
 
 interface Todo {
--- type-challenges/questions/00018-easy-tuple-length/template.ts
+++ type-challenges/questions/00018-easy-tuple-length/template.ts
@@ -1,1 +1,23 @@
-type Length<T> = any
+/* 获取tuple长度 */
+/* 解法 */
+
+/* js */
+function Len(arr) {
+  if (!Array.isArray(arr)) throw new Error('arr is not a array')
+
+  return arr.length
+}
+
+type Length<T extends readonly any[]> = T['length']
+
+/* 知识点 */
+/* tuple：item类型定死、长度固定的数组 */
+/* https://www.typescriptlang.org/docs/handbook/2/objects.html#tuple-types */
+
+/* 获取数组类型长度和元组类型长度的差异 */
+
+type TupleA = [string, number]
+type TupleALen = TupleA['length'] // 2；元组返回具体值
+
+type StrArr = string[]
+type StrArrLen = StrArr['length'] // number；
