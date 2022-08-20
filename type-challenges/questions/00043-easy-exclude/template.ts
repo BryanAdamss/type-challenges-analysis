026b4156e69dcfdf1fe93c712de1635726c443e1
bryanadamss <bryanadamss@foxmail.com>
2022-08-20 17:19:35 +0800

    feat: 00043-easy-exclude

--- type-challenges/questions/00043-easy-exclude/template.ts
+++ type-challenges/questions/00043-easy-exclude/template.ts
@@ -1,1 +1,32 @@
-type MyExclude<T, U> = any
+/* js */
+function MyExclude(T, U) {
+  const ret = []
+  T.forEach(item => {
+    !U.includes(item) && ret.push(item)
+  })
+
+  return ret
+}
+
+/* ts */
+type MyExclude<T, U> = T extends U ? never : T
+
+/* 重点 */
+// 1、联合类型extends时，会逐项对比
+// T : [1,2,3] U: [1]
+// T extends U ? never : T
+// 会进行如何对比
+// T      U   返回
+// 1 <--> 1   never->剔除
+// 2 <--> 1   2
+// 3 <--> 1   3
+// 最终返回[2,3]
+
+type MyExclude2<T, U> = T extends U ? '1' : '2'
+
+type t1 = MyExclude2<'a' | 'b' | 'c', 'a'> // '1' | '2'
+// T    U   返回
+// 'a'  'a' '1'
+// 'b'  'a' '2'
+// 'c'  'a' '2'
+// 最终返回'1'|'2'
