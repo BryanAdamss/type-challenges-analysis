fe601d52641c5e5900731e388e848f0d13891bf4
BryanAdamss <bryanadamss@foxmail.com>
2023-08-03 20:52:28 +0800

    feat: 00533-easy-concat

--- type-challenges/questions/00189-easy-awaited/template.ts
+++ type-challenges/questions/00189-easy-awaited/template.ts
@@ -3,3 +3,5 @@ type MyAwaited<T extends Promise<any>> = T extends Promise<infer R>
     ? MyAwaited<R>
     : R
   : never
+
+// 考虑嵌套场景,递归查询
--- type-challenges/questions/00533-easy-concat/template.ts
+++ type-challenges/questions/00533-easy-concat/template.ts
@@ -1,1 +1,4 @@
-type Concat<T, U> = any
+type Concat<T extends any[], U extends any[]> = [...T, ...U]
+
+// T extends any[]：用来限制T是一个数组，如果传递非数组会报错，U也是一样的道理。
+// [...T, ...U]：可以理解成JavaScript的扩展运算符...。
