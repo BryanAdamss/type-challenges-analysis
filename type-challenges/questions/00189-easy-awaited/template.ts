c5aa556331d99accb6fb7afddd00a2e18561613b
BryanAdamss <bryanadamss@foxmail.com>
2023-08-03 20:47:31 +0800

    feat: 00189-easy-awaited

--- type-challenges/questions/00189-easy-awaited/template.ts
+++ type-challenges/questions/00189-easy-awaited/template.ts
@@ -1,1 +1,5 @@
-type MyAwaited = any
+type MyAwaited<T extends Promise<any>> = T extends Promise<infer R>
+  ? R extends Promise<any>
+    ? MyAwaited<R>
+    : R
+  : never
--- type-challenges/questions/00189-easy-awaited/test-cases.ts
+++ type-challenges/questions/00189-easy-awaited/test-cases.ts
@@ -3,11 +3,15 @@ import type { Equal, Expect } from '@type-challenges/utils'
 type X = Promise<string>
 type Y = Promise<{ field: number }>
 type Z = Promise<Promise<string | number>>
+type ZZ = Promise<Promise<Promise<string | symbol>>>
+type ZZZ = Promise<Promise<Promise<Promise<string | symbol>>>>
 
 type cases = [
   Expect<Equal<MyAwaited<X>, string>>,
   Expect<Equal<MyAwaited<Y>, { field: number }>>,
   Expect<Equal<MyAwaited<Z>, string | number>>,
+  Expect<Equal<MyAwaited<ZZ>, string | symbol>>,
+  Expect<Equal<MyAwaited<ZZZ>, string | symbol>>
 ]
 
 // @ts-expect-error
