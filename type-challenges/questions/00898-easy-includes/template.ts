3f95616204c3754c9f1c4913e8296f6734f9050d
BryanAdamss <bryanadamss@foxmail.com>
2023-08-03 21:28:48 +0800

    feat: 00898-easy-includes

--- type-challenges/questions/00898-easy-includes/template.ts
+++ type-challenges/questions/00898-easy-includes/template.ts
@@ -1,1 +1,15 @@
-type Includes<T extends readonly any[], U> = any
+// 这种不够完善,不是严格相等
+// type Includes<T extends readonly any[], U> = U extends T[number] ? true : false
+
+/** 参考 https://github.com/microsoft/TypeScript/issues/27024#issuecomment-421529650 */
+type MyEqual<T, U> = (<Z>() => Z extends T ? 1 : 2) extends <Z>() => Z extends U
+  ? 1
+  : 2
+  ? true
+  : false
+
+type Includes<T extends readonly any[], U> = T extends [infer F, ...infer L]
+  ? MyEqual<F, U> extends true
+    ? true
+    : Includes<L, U>
+  : false
