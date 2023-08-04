98543a75ac9302b7b52f9d849797a06143a34a51
BryanAdamss <bryanadamss@foxmail.com>
2023-08-04 14:21:04 +0800

    feat: 03312-easy-parameters

--- type-challenges/questions/03312-easy-parameters/template.ts
+++ type-challenges/questions/03312-easy-parameters/template.ts
@@ -1,1 +1,6 @@
-type MyParameters<T extends (...args: any[]) => any> = any
+/** 通过infer 提取需要的内容 */
+type MyParameters<T extends (...args: any[]) => any> = T extends (
+  ...args: infer R
+) => any
+  ? R
+  : never
