ea38560b4d4729bb96e358c05b51b070b127c67b
BryanAdamss <bryanadamss@foxmail.com>
2023-08-04 16:34:01 +0800

    feat: 00012-medium-chainable-options

--- type-challenges/questions/00012-medium-chainable-options/template.ts
+++ type-challenges/questions/00012-medium-chainable-options/template.ts
@@ -1,4 +1,9 @@
-type Chainable = {
-  option(key: string, value: any): any
-  get(): any
+type Chainable</** 泛型 T当临时存储的变量用,默认为{}*/ T = {}> = {
+  option: <K extends string, V>(
+    key: K extends keyof T ? never : K /** 限制重复 key 名 */,
+    value: V
+  ) => /** 返回新增Record<K,V>的 Chainable 对象 */ Chainable<
+    Omit<T, K> & Record<K, V>
+  >
+  get: () => T
 }
