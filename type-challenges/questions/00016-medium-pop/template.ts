96a69336e26b09bc83feb100f4c2e88fb70d95c7
BryanAdamss <bryanadamss@foxmail.com>
2023-08-04 16:43:58 +0800

    feat: 00016-medium-pop

--- type-challenges/questions/00016-medium-pop/template.ts
+++ type-challenges/questions/00016-medium-pop/template.ts
@@ -1,1 +1,4 @@
-type Pop<T extends any[]> = any
+type MyPop<T extends any[]> = T extends [...infer F, infer L] ? F : never
+
+/** 答案:https://github.com/type-challenges/type-challenges/issues/37#issuecomment-687871052 */
+type Pop<T extends unknown[]> = T extends [...infer F, unknown] ? F : never
