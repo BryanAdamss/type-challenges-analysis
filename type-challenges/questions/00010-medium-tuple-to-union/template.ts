5ceb88fa87b6066a83089a6fd97436eef937afbe
BryanAdamss <bryanadamss@foxmail.com>
2023-08-04 15:33:27 +0800

    feat: 00010-medium-tuple-to-union

--- type-challenges/questions/00010-medium-tuple-to-union/template.ts
+++ type-challenges/questions/00010-medium-tuple-to-union/template.ts
@@ -1,1 +1,5 @@
-type TupleToUnion<T> = any
+/** https://github.com/type-challenges/type-challenges/issues/7 */
+type TupleToUnion<T extends any[]> = T[number]
+
+/** 或者 */
+type TupleToUnion2<T> = T extends Array<infer ITEMS> ? ITEMS : never
--- type-challenges/questions/00010-medium-tuple-to-union/test-cases.ts
+++ type-challenges/questions/00010-medium-tuple-to-union/test-cases.ts
@@ -2,5 +2,7 @@ import type { Equal, Expect } from '@type-challenges/utils'
 
 type cases = [
   Expect<Equal<TupleToUnion<[123, '456', true]>, 123 | '456' | true>>,
-  Expect<Equal<TupleToUnion<[123]>, 123>>,
+  Expect<Equal<TupleToUnion<[123]>, 123>>
 ]
+
+type Ret = TupleToUnion<[123, '456']>
