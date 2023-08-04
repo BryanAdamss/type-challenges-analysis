64fa368a5b304c6e04479e17e05814ff9aaf146b
BryanAdamss <bryanadamss@foxmail.com>
2023-08-04 17:14:52 +0800

    feat: 00020-medium-promise-all

--- type-challenges/questions/00020-medium-promise-all/template.ts
+++ type-challenges/questions/00020-medium-promise-all/template.ts
@@ -1,1 +1,7 @@
-declare function PromiseAll(values: any): any
+/** https://github.com/type-challenges/type-challenges/issues/211#issuecomment-1528824194 */
+
+declare function PromiseAll<T extends unknown[] /** 支持 case4 */>(
+  values: readonly [...T] /** 获取 T 类型 */
+): Promise<{
+  [K in keyof T]: Awaited<T[K]> /** 对嵌套的 Promise 做类型提取 */
+}>
