7acbd1df640a93652299c48575cfda5b158456ca
BryanAdamss <bryanadamss@foxmail.com>
2023-08-08 20:00:15 +0800

    feat: 00191-medium-append-argument

--- type-challenges/questions/00191-medium-append-argument/template.ts
+++ type-challenges/questions/00191-medium-append-argument/template.ts
@@ -1,1 +1,12 @@
-type AppendArgument<Fn, A> = any
+// 我的实现里...预算符使用错误
+// type AppendArgument<Fn extends (...args) => unknown, A> = Fn extends (
+//   ...args: infer P
+// ) => infer R
+//   ? (...(P|A)) => R
+//   : Fn
+
+type AppendArgument<Fn extends (...args) => unknown, A> = Fn extends (
+  ...args: infer P
+) => infer R
+  ? (...args: [...P, A]) => R
+  : Fn
