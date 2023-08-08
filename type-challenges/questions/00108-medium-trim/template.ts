2f4c67cd7feea91c18200b69be9cdd1bdc4968c8
BryanAdamss <bryanadamss@foxmail.com>
2023-08-08 19:24:19 +0800

    feat: 00108-medium-trim

--- type-challenges/questions/00108-medium-trim/template.ts
+++ type-challenges/questions/00108-medium-trim/template.ts
@@ -1,1 +1,5 @@
-type Trim<S extends string> = any
+type MyTrimRight<S extends string> = S extends `${infer L}${Space}`
+  ? MyTrimRight<L>
+  : S
+
+type Trim<S extends string> = MyTrimRight<TrimLeft<S>>
