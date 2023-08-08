556ea9286fb31ec487f9d74a25e4213d2336b6e1
BryanAdamss <bryanadamss@foxmail.com>
2023-08-08 19:16:36 +0800

    feat: 00106-medium-trimleft

--- type-challenges/questions/00106-medium-trimleft/template.ts
+++ type-challenges/questions/00106-medium-trimleft/template.ts
@@ -1,1 +1,7 @@
-type TrimLeft<S extends string> = any
+/** ts 中也可以使用模板字符串 */
+
+type Space = `\n` | '\t' | ' '
+
+type TrimLeft<S extends string> = S extends `${Space}${infer R}`
+  ? TrimLeft<R>
+  : S
--- type-challenges/questions/00106-medium-trimleft/test-cases.ts
+++ type-challenges/questions/00106-medium-trimleft/test-cases.ts
@@ -7,5 +7,5 @@ type cases = [
   Expect<Equal<TrimLeft<'     str     '>, 'str     '>>,
   Expect<Equal<TrimLeft<'   \n\t foo bar '>, 'foo bar '>>,
   Expect<Equal<TrimLeft<''>, ''>>,
-  Expect<Equal<TrimLeft<' \n\t'>, ''>>,
+  Expect<Equal<TrimLeft<' \n\t'>, ''>>
 ]
