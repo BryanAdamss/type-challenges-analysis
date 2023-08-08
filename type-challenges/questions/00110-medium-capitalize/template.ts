8b80dc79e1a4e473197a8f913545d394bf2f3d33
BryanAdamss <bryanadamss@foxmail.com>
2023-08-08 19:30:04 +0800

    feat: 00110-medium-capitalize

--- type-challenges/questions/00110-medium-capitalize/template.ts
+++ type-challenges/questions/00110-medium-capitalize/template.ts
@@ -1,3 +1,38 @@
-type MyCapitalize<S extends string> = S extends `${infer First}${infer Rest}`
+type MyCapitalize2<S extends string> = S extends `${infer First}${infer Rest}`
   ? `${Uppercase<First>}${Rest}`
   : S
+
+interface ToUpperCase {
+  a: 'A'
+  b: 'B'
+  c: 'C'
+  d: 'D'
+  e: 'E'
+  f: 'F'
+  g: 'G'
+  h: 'H'
+  i: 'I'
+  j: 'J'
+  k: 'K'
+  l: 'L'
+  m: 'M'
+  n: 'N'
+  o: 'O'
+  p: 'P'
+  q: 'Q'
+  r: 'R'
+  s: 'S'
+  t: 'T'
+  u: 'U'
+  v: 'V'
+  w: 'W'
+  x: 'X'
+  y: 'Y'
+  z: 'Z'
+}
+
+type LowerCase = keyof ToUpperCase
+type MyCapitalize<S extends string> =
+  S extends `${infer First extends LowerCase}${infer Rest}`
+    ? `${ToUpperCase[First]}${Rest}`
+    : S
