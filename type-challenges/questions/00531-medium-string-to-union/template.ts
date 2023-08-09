9a472f7ab6a3f17dede9d3bbb243057ea89e6c26
BryanAdamss <bryanadamss@foxmail.com>
2023-08-09 15:40:53 +0800

    feat: 00531-medium-string-to-union

--- type-challenges/questions/00531-medium-string-to-union/template.ts
+++ type-challenges/questions/00531-medium-string-to-union/template.ts
@@ -1,1 +1,3 @@
-type StringToUnion<T extends string> = any
+type StringToUnion<T extends string> = T extends `${infer F}${infer Rest}`
+  ? F | StringToUnion<Rest> /** 知识点,extends语句中可以用联合直接拼接 */
+  : never
