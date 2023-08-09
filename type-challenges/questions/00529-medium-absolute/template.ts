999cdd437ede1636725e6a63a18b77a44061abf1
BryanAdamss <bryanadamss@foxmail.com>
2023-08-09 15:29:47 +0800

    feat: 00529-medium-absolute

--- type-challenges/questions/00529-medium-absolute/template.ts
+++ type-challenges/questions/00529-medium-absolute/template.ts
@@ -1,1 +1,7 @@
-type Absolute<T extends number | string | bigint> = any
+type Absolute<T extends number | string | bigint> =
+  `${T}` extends `-${infer R}` /** 知识点,使用字符串模板将 number 转为string */
+    ? R
+    : `${T}`
+
+type AB = -1_000_000n
+type BC = `${AB}` // "-1000000"
