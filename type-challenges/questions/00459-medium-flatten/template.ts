a9a8b0e2932f5ce6b65f528200b4f9a72e07bcbc
BryanAdamss <bryanadamss@foxmail.com>
2023-08-09 15:06:42 +0800

    feat: 00459-medium-flatten

--- type-challenges/questions/00459-medium-flatten/template.ts
+++ type-challenges/questions/00459-medium-flatten/template.ts
@@ -1,1 +1,11 @@
-type Flatten = any
+type MyFlatten<T extends unknown[], A extends unknown[] = []> = T extends [
+  infer First,
+  ...infer Rest
+]
+  ? First extends unknown[]
+    ? MyFlatten<
+        [...First, ...Rest] /** 知识点 ...类似 js 中可以给数组降纬 */,
+        A
+      >
+    : MyFlatten<Rest, [...A, First]>
+  : A
--- type-challenges/questions/00459-medium-flatten/test-cases.ts
+++ type-challenges/questions/00459-medium-flatten/test-cases.ts
@@ -1,9 +1,17 @@
 import type { Equal, Expect } from '@type-challenges/utils'
 
 type cases = [
-  Expect<Equal<Flatten<[]>, []>>,
-  Expect<Equal<Flatten<[1, 2, 3, 4]>, [1, 2, 3, 4]>>,
-  Expect<Equal<Flatten<[1, [2]]>, [1, 2]>>,
-  Expect<Equal<Flatten<[1, 2, [3, 4], [[[5]]]]>, [1, 2, 3, 4, 5]>>,
-  Expect<Equal<Flatten<[{ foo: 'bar'; 2: 10 }, 'foobar']>, [{ foo: 'bar'; 2: 10 }, 'foobar']>>,
+  Expect<Equal<MyFlatten<[]>, []>>,
+  Expect<Equal<MyFlatten<[1, 2, 3, 4]>, [1, 2, 3, 4]>>,
+  Expect<Equal<MyFlatten<[1, [2]]>, [1, 2]>>,
+  Expect<Equal<MyFlatten<[1, 2, [3, 4], [[[5]]]]>, [1, 2, 3, 4, 5]>>,
+  Expect<
+    Equal<
+      MyFlatten<[{ foo: 'bar'; 2: 10 }, 'foobar']>,
+      [{ foo: 'bar'; 2: 10 }, 'foobar']
+    >
+  >
 ]
+
+// @ts-expect-error
+type error = MyFlatten<'1'>
