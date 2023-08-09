a7ecc2fb90041c19dbdb9bc68d2bd3492def2ba5
BryanAdamss <bryanadamss@foxmail.com>
2023-08-09 14:46:23 +0800

    feat: 00298-medium-length-of-string

--- type-challenges/questions/00298-medium-length-of-string/template.ts
+++ type-challenges/questions/00298-medium-length-of-string/template.ts
@@ -1,1 +1,6 @@
-type LengthOfString<S extends string> = any
+type LengthOfString1<
+  S extends string,
+  T extends string[] = [] /** 知识点1,设置临时变量存储数组,默认为[] */
+> = S extends `${infer F}${infer R}`
+  ? LengthOfString1<R, [...T, F]> /** 知识点2,利用[...T,F]向数组添加元素 */
+  : T['length'] /** 知识点3,通过将字符转为数组,获取 length 获取长度 */
--- type-challenges/questions/00298-medium-length-of-string/test-cases.ts
+++ type-challenges/questions/00298-medium-length-of-string/test-cases.ts
@@ -1,8 +1,10 @@
 import type { Equal, Expect } from '@type-challenges/utils'
 
+type A = LengthOfString1<'hello'>
+
 type cases = [
-  Expect<Equal<LengthOfString<''>, 0>>,
-  Expect<Equal<LengthOfString<'kumiko'>, 6>>,
-  Expect<Equal<LengthOfString<'reina'>, 5>>,
-  Expect<Equal<LengthOfString<'Sound! Euphonium'>, 16>>,
+  Expect<Equal<LengthOfString1<''>, 0>>,
+  Expect<Equal<LengthOfString1<'kumiko'>, 6>>,
+  Expect<Equal<LengthOfString1<'reina'>, 5>>,
+  Expect<Equal<LengthOfString1<'Sound! Euphonium'>, 16>>
 ]
