4d9a03502d43a1fc924a080f6a8e2d984b65c9b7
BryanAdamss <bryanadamss@foxmail.com>
2023-08-09 15:20:03 +0800

    feat: 00527-medium-append-to-object

--- type-challenges/questions/00296-medium-permutation/template.ts
+++ type-challenges/questions/00296-medium-permutation/template.ts
@@ -14,7 +14,7 @@ type MyIsNever2<T> = T extends never ? true : false
 type A = MyIsNever2<'str'> //str
 type B = MyIsNever2<never> // never  MyIsNever2<never> 中的 never 实际上是一个空的联合类型，一项都没有，所以 T extends ... 过程实际上被整体跳过了，所以最后的结果就是 never。
 
-/** 知识点2:遍历联合类型的范式,extends 自身即可 */
+/** 知识点2:直接遍历联合类型自身的范式,extends 自身即可 */
 type MyItrUnion<T> = T extends T ? [T] : never
 /** 知识点3:运用了分布式条件类型:在 条件类型 中使用 泛型参数 时，如果泛型参数是 联合类型，则会产生 distributive 的效果。 */
 /** https://www.typescriptlang.org/docs/handbook/2/conditional-types.html#distributive-conditional-types */
--- type-challenges/questions/00527-medium-append-to-object/template.ts
+++ type-challenges/questions/00527-medium-append-to-object/template.ts
@@ -1,1 +1,19 @@
-type AppendToObject<T, U, V> = any
+/**  使用&会报错 */
+// Equal<{name: 'john', age: 100}, {name: 'john', age: 100}> // => true
+// Equal<{name: 'john', age: 100}, {name: 'john'} & {age: 100}> // => false
+// type MyAppendToObject<
+//   T extends object,
+//   U extends string | number | symbol,
+//   V extends unknown
+// > = {
+//   [K in keyof T]: T[K]
+// } & { [K in U]: V }
+
+type AppendToObject<
+  T extends object,
+  K extends string | number | symbol,
+  V extends unknown
+> = {
+  /** 知识点,在对象中遍历 key 时用 keyof+in 组合 */
+  [P in keyof T | K]: P extends keyof T ? T[P] : V
+}
