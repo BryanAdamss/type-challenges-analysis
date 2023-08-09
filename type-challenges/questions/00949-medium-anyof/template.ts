3252e88e937d6067d0bcc151bfda8e7764d8eab8
BryanAdamss <bryanadamss@foxmail.com>
2023-08-09 17:03:54 +0800

    feat: 00949-medium-anyof

--- type-challenges/questions/00949-medium-anyof/template.ts
+++ type-challenges/questions/00949-medium-anyof/template.ts
@@ -1,1 +1,15 @@
-type AnyOf<T extends readonly any[]> = any
+/** 知识点,没有属性的对象不能用{}判断,  需要用{ [key: string]: never }*/
+type t3 = { name: 'test' } extends {} ? true : false // true
+type t4 = { name: 'test' } extends { [key: string]: never } ? true : false // false
+type t5 = {} extends { [key: string]: never } ? true : false // true
+
+type MyFalsy =
+  | false
+  | 0
+  | ''
+  | []
+  | {
+      [key: string]: never
+    }
+
+type AnyOf<T extends readonly any[]> = T[number] extends MyFalsy ? false : true
