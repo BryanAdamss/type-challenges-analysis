37e3e342a7c406eca5924d0bd5d3579f496a1da0
BryanAdamss <bryanadamss@foxmail.com>
2023-08-09 17:09:31 +0800

    feat: 01042-medium-isnever

--- type-challenges/questions/01042-medium-isnever/template.ts
+++ type-challenges/questions/01042-medium-isnever/template.ts
@@ -1,1 +1,3 @@
-type IsNever = any
+/** 原理见: questions/00296-medium-permutation */
+/** https://www.typescriptlang.org/docs/handbook/2/conditional-types.html#distributive-conditional-types */
+type IsNever<T> = [T] extends [never] ? true : false
