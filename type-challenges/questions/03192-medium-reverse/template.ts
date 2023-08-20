cc9e93dd638e7be687795f100f787f4e8e34d21e
BryanAdamss <bryanadamss@foxmail.com>
2023-08-21 07:28:49 +0800

    feat: 03192-medium-reverse

--- type-challenges/questions/03192-medium-reverse/template.ts
+++ type-challenges/questions/03192-medium-reverse/template.ts
@@ -1,1 +1,4 @@
-type Reverse<T> = any
+/** 利用 infer + ...实现顺序交换 */
+type Reverse<T extends unknown[]> = T extends [infer F, ...infer R]
+  ? [...Reverse<R>, F]
+  : T
--- type-challenges/questions/03192-medium-reverse/test-cases.ts
+++ type-challenges/questions/03192-medium-reverse/test-cases.ts
@@ -3,5 +3,12 @@ import type { Equal, Expect } from '@type-challenges/utils'
 type cases = [
   Expect<Equal<Reverse<[]>, []>>,
   Expect<Equal<Reverse<['a', 'b']>, ['b', 'a']>>,
-  Expect<Equal<Reverse<['a', 'b', 'c']>, ['c', 'b', 'a']>>,
+  Expect<Equal<Reverse<['a', 'b', 'c']>, ['c', 'b', 'a']>>
+]
+
+type errors = [
+  // @ts-expect-error
+  Reverse<'string'>,
+  // @ts-expect-error
+  Reverse<{ key: 'value' }>
 ]
