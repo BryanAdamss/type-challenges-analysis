81f1cda35494c0d9a14b2331255614ebe3b4e913
BryanAdamss <bryanadamss@foxmail.com>
2023-08-21 06:48:12 +0800

    feat: 02946-medium-objectentries

--- type-challenges/questions/02946-medium-objectentries/template.ts
+++ type-challenges/questions/02946-medium-objectentries/template.ts
@@ -1,1 +1,10 @@
-type ObjectEntries<T> = any
+/** 知识点,对象转联合,用 T[keyof T] */
+type ObjectToUnion<T> = T[keyof T]
+
+/** 知识点,数组转联合类型用下标*/
+// ['1', '2']['number'] // '1' | '2'
+
+/** 参考:https://github.com/type-challenges/type-challenges/issues/14052 */
+type ObjectEntries<T> = {
+  [Key in keyof T]: [Key, T[Key]]
+}[keyof T]
--- type-challenges/questions/02946-medium-objectentries/test-cases.ts
+++ type-challenges/questions/02946-medium-objectentries/test-cases.ts
@@ -6,11 +6,22 @@ interface Model {
   locations: string[] | null
 }
 
-type ModelEntries = ['name', string] | ['age', number] | ['locations', string[] | null]
+type ModelEntries =
+  | ['name', string]
+  | ['age', number]
+  | ['locations', string[] | null]
+
+type Ret = ObjectEntries<Model>
 
 type cases = [
   Expect<Equal<ObjectEntries<Model>, ModelEntries>>,
   Expect<Equal<ObjectEntries<Partial<Model>>, ModelEntries>>,
   Expect<Equal<ObjectEntries<{ key?: undefined }>, ['key', undefined]>>,
   Expect<Equal<ObjectEntries<{ key: undefined }>, ['key', undefined]>>,
+  Expect<
+    Equal<
+      ObjectEntries<{ key: string | undefined }>,
+      ['key', string | undefined]
+    >
+  >
 ]
