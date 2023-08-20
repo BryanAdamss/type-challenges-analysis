a32cd7cff6e92946da09d16ac06b7a4d6101a18a
BryanAdamss <bryanadamss@foxmail.com>
2023-08-20 15:18:59 +0800

    feat: 02688-medium-startswith

--- type-challenges/questions/02688-medium-startswith/template.ts
+++ type-challenges/questions/02688-medium-startswith/template.ts
@@ -1,1 +1,11 @@
-type StartsWith<T extends string, U extends string> = any
+/** 符合这种模式,即视为是以 U 开头 */
+type MyStartsWith<
+  T extends string,
+  U extends string
+> = T extends `${U}${infer R}` ? true : false
+
+/** 官方解法:https://github.com/type-challenges/type-challenges/issues/2690 */
+/** 知识点,模板字符串匹配时,可不用 infer 接收,用string表明后面是 string 即可 */
+type StartsWith<T extends string, U extends string> = T extends `${U}${string}`
+  ? true
+  : false
