d320b69325fabb294285f9f1517d4ceb01c62334
BryanAdamss <bryanadamss@foxmail.com>
2023-08-22 07:54:34 +0800

    feat: 04179-medium-flip

--- type-challenges/questions/04179-medium-flip/template.ts
+++ type-challenges/questions/04179-medium-flip/template.ts
@@ -1,1 +1,5 @@
-type Flip<T> = any
+/** 解法:https://github.com/type-challenges/type-challenges/issues/14094 */
+type Flip<T extends Record<string, string | number | boolean>> = {
+  /** 知识点,通过模板字符串,将 boolean 转换为 string 做为key 用 */
+  [P in keyof T as `${T[P]}`]: P
+}
--- type-challenges/questions/04179-medium-flip/test-cases.ts
+++ type-challenges/questions/04179-medium-flip/test-cases.ts
@@ -4,5 +4,9 @@ type cases = [
   Expect<Equal<{ a: 'pi' }, Flip<{ pi: 'a' }>>>,
   Expect<NotEqual<{ b: 'pi' }, Flip<{ pi: 'a' }>>>,
   Expect<Equal<{ 3.14: 'pi'; true: 'bool' }, Flip<{ pi: 3.14; bool: true }>>>,
-  Expect<Equal<{ val2: 'prop2'; val: 'prop' }, Flip<{ prop: 'val'; prop2: 'val2' }>>>,
+  Expect<
+    Equal<{ val2: 'prop2'; val: 'prop' }, Flip<{ prop: 'val'; prop2: 'val2' }>>
+  >
 ]
+
+type Ret = Flip<{ pi: 3.14; bool: true }>
