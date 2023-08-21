0afa044490f9993d20dd3213b37c06de8f8545bc
BryanAdamss <bryanadamss@foxmail.com>
2023-08-22 07:29:59 +0800

    feat: 03326-medium-bem-style-string

--- type-challenges/questions/03326-medium-bem-style-string/template.ts
+++ type-challenges/questions/03326-medium-bem-style-string/template.ts
@@ -1,1 +1,19 @@
-type BEM<B extends string, E extends string[], M extends string[]> = any
+/** 我的解法 */
+type MyBEM<
+  B extends string,
+  E extends string[],
+  M extends string[]
+> = E extends []
+  ? `${B}--${M[number]}`
+  : M extends []
+  ? `${B}__${E[number]}`
+  : `${B}__${E[number]}--${M[number]}`
+
+/** 官方解法 */
+type IsNever2<T> = [T] extends [never] ? true : false
+type IsUnion2<U> = IsNever<U> extends true ? '' : U
+type BEM<
+  B extends string,
+  E extends string[],
+  M extends string[]
+> = `${B}${IsUnion2<`__${E[number]}`>}${IsUnion2<`--${M[number]}`>}`
--- type-challenges/questions/03326-medium-bem-style-string/test-cases.ts
+++ type-challenges/questions/03326-medium-bem-style-string/test-cases.ts
@@ -2,6 +2,16 @@ import type { Equal, Expect } from '@type-challenges/utils'
 
 type cases = [
   Expect<Equal<BEM<'btn', ['price'], []>, 'btn__price'>>,
-  Expect<Equal<BEM<'btn', ['price'], ['warning', 'success']>, 'btn__price--warning' | 'btn__price--success' >>,
-  Expect<Equal<BEM<'btn', [], ['small', 'medium', 'large']>, 'btn--small' | 'btn--medium' | 'btn--large' >>,
+  Expect<
+    Equal<
+      BEM<'btn', ['price'], ['warning', 'success']>,
+      'btn__price--warning' | 'btn__price--success'
+    >
+  >,
+  Expect<
+    Equal<
+      BEM<'btn', [], ['small', 'medium', 'large']>,
+      'btn--small' | 'btn--medium' | 'btn--large'
+    >
+  >
 ]
