04273bb5019ca93fab9be89a12ae12e2dcd5d90d
BryanAdamss <bryanadamss@foxmail.com>
2023-08-22 07:45:31 +0800

    feat: 03376-medium-inordertraversal

--- type-challenges/questions/03376-medium-inordertraversal/template.ts
+++ type-challenges/questions/03376-medium-inordertraversal/template.ts
@@ -3,4 +3,15 @@ interface TreeNode {
   left: TreeNode | null
   right: TreeNode | null
 }
-type InorderTraversal<T extends TreeNode | null> = any
+
+/** 知识点,二叉树的中序遍历,https://zhuanlan.zhihu.com/p/56895993 */
+
+/** 解法:https://github.com/type-challenges/type-challenges/issues/5220 */
+type InorderTraversal<T extends TreeNode | null> =
+  /** 知识点,T 是联合类型,需要通过[]规避分配性 */ [T] extends [TreeNode]
+    ? [
+        ...InorderTraversal<T['left']>,
+        T['val'],
+        ...InorderTraversal<T['right']>
+      ]
+    : []
