7ead1da4d7b9130c0480f5358f43710c16da4ced
BryanAdamss <bryanadamss@foxmail.com>
2023-08-22 08:14:08 +0800

    feat: 04182-medium-fibonacci-sequence

--- type-challenges/questions/04182-medium-fibonacci-sequence/template.ts
+++ type-challenges/questions/04182-medium-fibonacci-sequence/template.ts
@@ -1,1 +1,13 @@
-type Fibonacci<T extends number> = any
+/** 解法: https://github.com/type-challenges/type-challenges/issues/16384 */
+type Fibonacci<
+  T extends number,
+  /** 斐波那契前2项都是固定的1, 从第3项开始 */
+  Prev extends number[] = [1],
+  Cur extends number[] = [1],
+  Count extends number[] = [1, 1, 1]
+> = T extends 1 | 2
+  ? 1
+  : /** 满足退出条件,返回长度*/ T extends Count['length']
+  ? [...Prev, ...Cur]['length']
+  : /** 不满足,则继续,调整 Prev 和 Cur值 已及 Count++ */
+    Fibonacci<T, Cur, [...Prev, ...Cur], [...Count, 1]>
