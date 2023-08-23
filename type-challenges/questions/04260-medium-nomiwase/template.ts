fad48f2d137b135496a1d0510636b09dec57aab1
BryanAdamss <bryanadamss@foxmail.com>
2023-08-24 07:25:43 +0800

    feat: 04260-medium-nomiwase

--- type-challenges/questions/04260-medium-nomiwase/template.ts
+++ type-challenges/questions/04260-medium-nomiwase/template.ts
@@ -1,1 +1,15 @@
-type AllCombinations<S> = any
+/** 知识点,利用 infer + | 将字符串转换为联合类型,"AB"->""|"A"|"B" */
+type StringToUnion2<S> = S extends `${infer F}${infer R}`
+  ? F | StringToUnion2<R>
+  : S
+
+/** 解法:https://github.com/type-challenges/type-challenges/issues/16430 */
+type AllCombinations<
+  S extends string,
+  T extends string = StringToUnion2<S>,
+  U extends string = T
+> = S extends `${infer F}${infer R}`
+  ? /** 遍历联合类型 U */ U extends U
+    ? `${U}${AllCombinations<R, U extends '' ? T : Exclude<T, U>>}`
+    : never
+  : ''
