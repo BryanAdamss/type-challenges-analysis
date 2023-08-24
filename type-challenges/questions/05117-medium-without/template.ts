8a4710d2c662ac2c9ec5fb63ab9564853a338b4e
BryanAdamss <bryanadamss@foxmail.com>
2023-08-24 16:24:34 +0800

    feat: 05117-medium-without

--- type-challenges/questions/05117-medium-without/template.ts
+++ type-challenges/questions/05117-medium-without/template.ts
@@ -1,1 +1,9 @@
-type Without<T, U> = any
+/** 知识点,数组转 union */
+type ToUnion<T> = T extends any[] ? T[number] : T
+
+/** 参考:https://github.com/type-challenges/type-challenges/issues/14118 */
+type Without<T, U> = T extends [infer R, ...infer F]
+  ? R extends ToUnion<U>
+    ? Without<F, U>
+    : [R, ...Without<F, U>]
+  : T
