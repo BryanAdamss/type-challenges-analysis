a5db9e77c516c7c3e00bd598c4443346a97f7881
BryanAdamss <bryanadamss@foxmail.com>
2023-08-24 15:17:19 +0800

    feat: 04499-medium-chunk

--- type-challenges/questions/04499-medium-chunk/template.ts
+++ type-challenges/questions/04499-medium-chunk/template.ts
@@ -1,1 +1,15 @@
-type Chunk = any
+/** 参考:https://github.com/type-challenges/type-challenges/issues/16479 */
+type Chunk<
+  T extends unknown[],
+  U extends number,
+  /** 中间状态的单独 chunk */
+  SingleChunk extends unknown[] = [],
+  /** 最终数组 */
+  Ret extends unknown[] = []
+> = T extends [infer F, ...infer R]
+  ? SingleChunk['length'] extends U
+    ? Chunk<R, U, [F], [...Ret, SingleChunk]>
+    : Chunk<R, U, [...SingleChunk, F], Ret>
+  : /** 兼容T 为[]场景*/ SingleChunk['length'] extends 0
+  ? Ret
+  : [...Ret, SingleChunk]
--- type-challenges/questions/04499-medium-chunk/test-cases.ts
+++ type-challenges/questions/04499-medium-chunk/test-cases.ts
@@ -6,5 +6,5 @@ type cases = [
   Expect<Equal<Chunk<[1, 2, 3], 2>, [[1, 2], [3]]>>,
   Expect<Equal<Chunk<[1, 2, 3, 4], 2>, [[1, 2], [3, 4]]>>,
   Expect<Equal<Chunk<[1, 2, 3, 4], 5>, [[1, 2, 3, 4]]>>,
-  Expect<Equal<Chunk<[1, true, 2, false], 2>, [[1, true], [2, false]]>>,
+  Expect<Equal<Chunk<[1, true, 2, false], 2>, [[1, true], [2, false]]>>
 ]
