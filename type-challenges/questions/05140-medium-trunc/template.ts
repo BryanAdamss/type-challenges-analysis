8df65c200fe7ebb7d9a0642ece4d98da55ab211e
BryanAdamss <bryanadamss@foxmail.com>
2023-08-24 16:29:52 +0800

    feat: 05140-medium-trunc

--- type-challenges/questions/05140-medium-trunc/template.ts
+++ type-challenges/questions/05140-medium-trunc/template.ts
@@ -1,1 +1,6 @@
-type Trunc = any
+/** 知识点,通过模板字符串可以直接将 number 转换为字符串类型 */
+type Trunc<S extends string | number> = `${S}` extends `${infer R}.${infer U}`
+  ? R extends ''
+    ? '0'
+    : R
+  : `${S}`
