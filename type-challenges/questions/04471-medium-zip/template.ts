6cfb2b28044a83ba6f3df8b8d6b1e87fd0c606db
BryanAdamss <bryanadamss@foxmail.com>
2023-08-24 14:40:02 +0800

    feat: 04471-medium-zip

--- type-challenges/questions/04471-medium-zip/template.ts
+++ type-challenges/questions/04471-medium-zip/template.ts
@@ -1,1 +1,8 @@
-type Zip = any
+/** 利用 infer 实现,参考:https://github.com/type-challenges/type-challenges/issues/4495 */
+/** 知识点,因为不是联合类型,无法使用分布式特性 */
+type Zip<A extends unknown[], B extends unknown[]> = [A, B] extends [
+  [infer AF, ...infer AR],
+  [infer BF, ...infer BR]
+]
+  ? [[AF, BF], ...Zip<AR, BR>]
+  : []
