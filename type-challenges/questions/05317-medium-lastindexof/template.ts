a2f4fe684b39418e9ae728b4ee9f09e852aee2ae
BryanAdamss <bryanadamss@foxmail.com>
2023-11-14 22:08:51 +0800

    feat: 05317-medium-lastindexof

--- type-challenges/questions/05317-medium-lastindexof/template.ts
+++ type-challenges/questions/05317-medium-lastindexof/template.ts
@@ -1,1 +1,11 @@
-type LastIndexOf<T, U> = any
+/** 解法:https://github.com/type-challenges/type-challenges/issues/5330#issuecomment-1506602140 */
+type LastIndexOf<T extends unknown[], U> = T extends [
+  /** 知识点:从后往前遍历数组 */
+  ...infer Rest,
+  infer Last
+]
+  ? MyEqual<Last, U> extends true
+    ? /** 知识点:用前面数组的 length 代表当前元素索引 */
+      Rest['length']
+    : LastIndexOf<Rest, U>
+  : -1
