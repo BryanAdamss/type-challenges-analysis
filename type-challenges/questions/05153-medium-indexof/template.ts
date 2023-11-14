c392e040392f1c1e9a2343d46611cf1eaddd05ae
BryanAdamss <bryanadamss@foxmail.com>
2023-11-14 21:46:28 +0800

    feat: 05153-medium-indexof

--- type-challenges/questions/05153-medium-indexof/template.ts
+++ type-challenges/questions/05153-medium-indexof/template.ts
@@ -1,1 +1,9 @@
-type IndexOf<T, U> = any
+/** 知识点,遇到数字相关通通用数组长度来代替 */
+type IndexOf<T extends unknown[], U, Count extends 1[] = []> = T extends [
+  infer First,
+  ...infer Rest
+]
+  ? MyEqual<U, First> extends true
+    ? Count['length']
+    : IndexOf<Rest, U, [...Count, 1]>
+  : -1
