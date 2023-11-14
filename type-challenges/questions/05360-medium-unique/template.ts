124344671db98895d66eb2a94a30441db3fc4a5e
BryanAdamss <bryanadamss@foxmail.com>
2023-11-14 22:17:08 +0800

    feat: 05360-medium-unique

--- type-challenges/questions/05360-medium-unique/template.ts
+++ type-challenges/questions/05360-medium-unique/template.ts
@@ -1,1 +1,9 @@
-type Unique<T> = any
+/** 解法1:借助 indexOf 判断数组中是否存在 */
+type Unique<T extends unknown[], Arr extends unknown[] = []> = T extends [
+  infer F,
+  ...infer R
+]
+  ? IndexOf<Arr, F> extends -1
+    ? Unique<R, [...Arr, F]>
+    : Unique<R, [...Arr]>
+  : Arr
