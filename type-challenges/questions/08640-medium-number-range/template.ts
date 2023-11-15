b889aee91a43c2fc03d2ec6a1441965b246e914b
BryanAdamss <bryanadamss@foxmail.com>
2023-11-15 21:51:43 +0800

    feat: 08640-medium-number-range

--- type-challenges/questions/08640-medium-number-range/template.ts
+++ type-challenges/questions/08640-medium-number-range/template.ts
@@ -1,1 +1,9 @@
-type NumberRange<L, H> = any
+type NumberRange<
+  L extends number,
+  H extends number,
+  /** 存储,若L 为0 则初始化[],否则至少为1,有两个数,则初始化为[1,1] */
+  Idx extends 1[] = L extends 0 ? [] : [1, 1],
+  Res = never
+> = Idx['length'] extends H
+  ? H | Res
+  : NumberRange<L, H, [...Idx, 1], Idx['length'] | Res>
