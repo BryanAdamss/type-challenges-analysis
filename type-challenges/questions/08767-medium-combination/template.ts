3a4801715237387a2edc41e3b2193300ec5951c6
BryanAdamss <bryanadamss@foxmail.com>
2023-11-15 22:07:30 +0800

    feat: 08767-medium-combination

--- type-challenges/questions/08767-medium-combination/template.ts
+++ type-challenges/questions/08767-medium-combination/template.ts
@@ -1,1 +1,8 @@
-type Combination<T extends string[]> = any
+/** 解法:https://github.com/type-challenges/type-challenges/issues/14157 */
+type Combination<
+  T extends string[],
+  All = T[number],
+  Item = All
+> = /** 重新将 Item 推断为 string,否则会报类型错误*/ Item extends infer I extends string
+  ? I | `${I} ${Combination<[], Exclude<All, I>>}`
+  : never
