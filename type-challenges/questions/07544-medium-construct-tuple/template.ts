9bc769113d505f7544ba6d166032d52c2944005d
BryanAdamss <bryanadamss@foxmail.com>
2023-11-15 21:31:48 +0800

    feat: 07544-medium-construct-tuple

--- type-challenges/questions/07544-medium-construct-tuple/template.ts
+++ type-challenges/questions/07544-medium-construct-tuple/template.ts
@@ -1,1 +1,5 @@
-type ConstructTuple<L extends number> = any
+/** 解法1,遇到数字想到构造数组取length来表示 */
+type ConstructTuple<
+  L extends number,
+  Ret extends unknown[] = []
+> = Ret['length'] extends L ? Ret : ConstructTuple<L, [...Ret, unknown]>
