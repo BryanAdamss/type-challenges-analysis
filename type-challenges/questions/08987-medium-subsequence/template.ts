33d2ef99f0fd61178a4c4d14130132d4bb3cd2a7
BryanAdamss <bryanadamss@foxmail.com>
2023-11-15 22:22:09 +0800

    feat: 08987-medium-subsequence

--- type-challenges/questions/08987-medium-subsequence/template.ts
+++ type-challenges/questions/08987-medium-subsequence/template.ts
@@ -1,1 +1,6 @@
-type Subsequence<T extends any[]> = any
+type Subsequence<T extends any[], Prefix extends any[] = []> = T extends [
+  infer F,
+  ...infer R
+]
+  ? Subsequence<R, Prefix | [...Prefix, F]>
+  : Prefix
