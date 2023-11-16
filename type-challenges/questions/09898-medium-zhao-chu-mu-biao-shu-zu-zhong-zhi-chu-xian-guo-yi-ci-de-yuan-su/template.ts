241687f9db791c91d7592e772341a6dbec54157f
BryanAdamss <bryanadamss@foxmail.com>
2023-11-16 22:47:25 +0800

    feat: 09898-medium-zhao-chu-mu-biao-shu-zu-zhong-zhi-chu-xian-guo-yi-ci-de-yuan-su

--- type-challenges/questions/09898-medium-zhao-chu-mu-biao-shu-zu-zhong-zhi-chu-xian-guo-yi-ci-de-yuan-su/template.ts
+++ type-challenges/questions/09898-medium-zhao-chu-mu-biao-shu-zu-zhong-zhi-chu-xian-guo-yi-ci-de-yuan-su/template.ts
@@ -1,1 +1,16 @@
-type FindEles<T extends any[]> = any
+/** 解法:https://github.com/type-challenges/type-challenges/issues/28152 */
+type FindEles<
+  T extends any[],
+  /** 存结果 */
+  U extends any[] = [],
+  /** 存计算过的数 */
+  O extends any[] = []
+> = T extends [infer F, ...infer Rest]
+  ? F extends [...Rest, ...O][number]
+    ? /** F在数组中出现过,则继续找 */ FindEles<Rest, U, [...O, F]>
+    : /** F在数组中未出现过,则保存到U 中,继续找*/ FindEles<
+        Rest,
+        [...U, F],
+        [...O, F]
+      >
+  : U
