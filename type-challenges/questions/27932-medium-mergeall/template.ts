dd38c0a1f8ad3683fb114cd40ed26289ac3db995
BryanAdamss <bryanadamss@foxmail.com>
2023-11-18 21:38:46 +0800

    feat: 27932-medium-mergeall

--- type-challenges/questions/27932-medium-mergeall/template.ts
+++ type-challenges/questions/27932-medium-mergeall/template.ts
@@ -1,1 +1,13 @@
-type MergeAll<XS> = any
+type MergeAll<XS, P = {}> = XS extends [infer F, ...infer Rest]
+  ? MergeAll<Rest, MyMerge<P, F>>
+  : P
+
+type MyMerge<F, S> = {
+  [P in keyof F | keyof S]: P extends keyof S
+    ? P extends keyof F
+      ? S[P] | F[P]
+      : S[P]
+    : P extends keyof F
+    ? F[P]
+    : never
+}
