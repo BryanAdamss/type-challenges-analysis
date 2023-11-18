4db78c964db72a6b4945f7f815a7d9b1284dafe2
BryanAdamss <bryanadamss@foxmail.com>
2023-11-18 21:01:36 +0800

    feat: 25170-medium-replace-first

--- type-challenges/questions/25170-medium-replace-first/template.ts
+++ type-challenges/questions/25170-medium-replace-first/template.ts
@@ -1,1 +1,8 @@
-type ReplaceFirst<T extends readonly unknown[], S, R> = any
+type ReplaceFirst<T extends readonly unknown[], From, To> = T extends [
+  infer First,
+  ...infer Rest
+]
+  ? First extends From
+    ? [To, ...Rest]
+    : [First, ...ReplaceFirst<Rest, From, To>]
+  : T
