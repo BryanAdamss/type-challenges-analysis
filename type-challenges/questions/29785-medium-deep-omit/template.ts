9bbe4c0fc635375ec592fe69b06863a1a979eaaf
BryanAdamss <bryanadamss@foxmail.com>
2023-11-18 21:44:01 +0800

    feat: 29785-medium-deep-omit

--- type-challenges/questions/29785-medium-deep-omit/template.ts
+++ type-challenges/questions/29785-medium-deep-omit/template.ts
@@ -1,1 +1,9 @@
-type DeepOmit = any
+type DeepOmit<T, Keys> = {
+  [K in keyof T as K extends Keys ? never : K]: K extends Keys
+    ? never
+    : Keys extends `${infer F}.${infer R}`
+    ? K extends F
+      ? DeepOmit<T[K], R>
+      : T[K]
+    : T[K]
+}
