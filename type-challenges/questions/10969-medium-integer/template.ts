a282f7e75b0daaa13a8d8a662a4f144a179b28b5
BryanAdamss <bryanadamss@foxmail.com>
2023-11-18 20:02:46 +0800

    feat: 10969-medium-integer

--- type-challenges/questions/10969-medium-integer/template.ts
+++ type-challenges/questions/10969-medium-integer/template.ts
@@ -1,1 +1,3 @@
-type Integer<T> = any
+/** 思路:先转成字符串,再和 bigint 比较 */
+/** 知识点,bingint 只能为整数https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/BigInt */
+type Integer<T extends number> = `${T}` extends `${bigint}` ? T : never
