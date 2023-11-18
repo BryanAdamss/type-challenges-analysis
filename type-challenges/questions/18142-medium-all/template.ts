c50119fb0997ef72971d1cd3355b0ea99b7a6ad4
BryanAdamss <bryanadamss@foxmail.com>
2023-11-18 20:18:30 +0800

    feat: 18142-medium-all

--- type-challenges/questions/18142-medium-all/template.ts
+++ type-challenges/questions/18142-medium-all/template.ts
@@ -1,1 +1,6 @@
-type All = any
+/** 思路:使用 isEqual 递归判断 */
+type All<T extends unknown[], Target> = T extends [infer F, ...infer R]
+  ? MyEqual<F, Target> extends true
+    ? All<R, Target>
+    : false
+  : true
