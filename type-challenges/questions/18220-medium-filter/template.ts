da09f77bf20b124ce85f9f7415ee161dcb017cc6
BryanAdamss <bryanadamss@foxmail.com>
2023-11-18 20:22:10 +0800

    feat: 18220-medium-filter

--- type-challenges/questions/18220-medium-filter/template.ts
+++ type-challenges/questions/18220-medium-filter/template.ts
@@ -1,1 +1,6 @@
-type Filter<T extends any[], P> = []
+/** 思路:递归遍历判断 */
+type Filter<T extends any[], P> = T extends [infer F, ...infer R]
+  ? F extends P
+    ? [F, ...Filter<R, P>]
+    : [...Filter<R, P>]
+  : []
