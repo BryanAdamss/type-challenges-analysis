b9a586764c88b077379df24163366c072b788645
BryanAdamss <bryanadamss@foxmail.com>
2023-11-18 21:05:44 +0800

    feat: 25270-medium-transpose

--- type-challenges/questions/25270-medium-transpose/template.ts
+++ type-challenges/questions/25270-medium-transpose/template.ts
@@ -1,1 +1,16 @@
-type Transpose<M extends number[][]> = any
+/** 解法:https://github.com/type-challenges/type-challenges/issues/28364 */
+type Temp<M extends number[][], I extends number> = M extends [
+  infer F extends number[],
+  ...infer Res extends number[][]
+]
+  ? [F[I], ...Temp<Res, I>]
+  : []
+
+type Transpose<M extends number[][], Res extends number[][] = []> = M extends [
+  infer F extends number[],
+  ...any
+]
+  ? F['length'] extends Res['length']
+    ? []
+    : [Temp<M, Res['length']>, ...Transpose<M, [...Res, any]>]
+  : []
