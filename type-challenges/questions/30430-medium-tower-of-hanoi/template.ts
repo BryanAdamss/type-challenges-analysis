38999dee95656471310f7ca16accab39adf931a9
BryanAdamss <bryanadamss@foxmail.com>
2023-11-18 21:47:00 +0800

    feat: 30430-medium-tower-of-hanoi

--- type-challenges/questions/30430-medium-tower-of-hanoi/template.ts
+++ type-challenges/questions/30430-medium-tower-of-hanoi/template.ts
@@ -1,1 +1,20 @@
-type Hanoi<N extends number, From = 'A', To = 'B', Intermediate = 'C'> = any
+type Hanoi<N extends number, From = 'A', To = 'B', Intermediate = 'C'> = Helper<
+  N,
+  [],
+  From,
+  To,
+  Intermediate
+>
+type Helper<
+  N extends number,
+  C extends 1[],
+  From extends unknown,
+  To extends unknown,
+  Intermediate extends unknown
+> = C['length'] extends N
+  ? []
+  : [
+      ...Helper<N, [...C, 1], From, Intermediate, To>,
+      [From, To],
+      ...Helper<N, [...C, 1], Intermediate, To, From>
+    ]
