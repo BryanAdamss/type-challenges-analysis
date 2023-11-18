4062a628b903b3cdfbc80c3639c93bee508316fc
BryanAdamss <bryanadamss@foxmail.com>
2023-11-18 20:37:36 +0800

    feat: 21104-medium-findall

--- type-challenges/questions/21104-medium-findall/template.ts
+++ type-challenges/questions/21104-medium-findall/template.ts
@@ -1,1 +1,17 @@
-type FindAll<T extends string, P extends string> = any
+type FindAll<
+  T extends string,
+  P extends string,
+  Result extends any[] = [],
+  Index extends any[] = []
+> = P extends ''
+  ? []
+  : T extends `${string}${infer Last}`
+  ? T extends `${P}${string}`
+    ? /** 找到目标字符串,则 Result 和 Index 都加1 */
+      FindAll<Last, P, [...Result, Index['length']], [...Index, 0]>
+    : FindAll<Last, P, Result, [...Index, 0]>
+  : Result
+
+type LL<T> = T extends `${string}${infer Last}` ? Last : T
+
+type Test11 = LL<'Hello'> // ello
