85148cc7de8a81ca2eb0172f95bdcbc0b2146382
BryanAdamss <bryanadamss@foxmail.com>
2023-11-18 20:10:52 +0800

    feat: 16259-medium-to-primitive

--- type-challenges/questions/16259-medium-to-primitive/template.ts
+++ type-challenges/questions/16259-medium-to-primitive/template.ts
@@ -1,1 +1,13 @@
-type ToPrimitive = any
+/** 思路:函数直接判断/对象递归遍历判断,基础类型用 valueOf 中的返回值确定 */
+type ToPrimitive<T> = T extends object
+  ? T extends (...args: any[]) => any
+    ? Function
+    : { [P in keyof T]: ToPrimitive<T[P]> }
+  : T extends { valueOf: () => infer R }
+  ? R
+  : T
+
+/** 知识点,ts中的 valueOf 可以返回类型值的原始类型 */
+type a = 3
+type b<T> = T extends { valueOf: () => infer R } ? R : T
+type c = b<a> // number
