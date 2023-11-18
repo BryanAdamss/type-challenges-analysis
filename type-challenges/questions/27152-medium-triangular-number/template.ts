719fa7a6f40a6280cc47a3efe7097ce6837464b8
BryanAdamss <bryanadamss@foxmail.com>
2023-11-18 21:20:17 +0800

    feat: 27152-medium-triangular-number

--- type-challenges/questions/27152-medium-triangular-number/template.ts
+++ type-challenges/questions/27152-medium-triangular-number/template.ts
@@ -1,1 +1,9 @@
-type Triangular<N extends number> = any
+type Triangular<
+  N extends number,
+  /** 存放每次累加的数组 */
+  Temp extends number[] = [],
+  /** 最终结果数组 */
+  Result extends number[] = []
+> = Temp['length'] extends N
+  ? Result['length']
+  : Triangular<N, [0, ...Temp], [...Result, ...Temp, 0]>
