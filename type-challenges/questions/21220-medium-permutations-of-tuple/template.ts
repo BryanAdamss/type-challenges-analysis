637b8d2c58e9096765a66262eb77865a6b86442a
BryanAdamss <bryanadamss@foxmail.com>
2023-11-18 20:58:33 +0800

    feat: 21220-medium-permutations-of-tuple

--- type-challenges/questions/21220-medium-permutations-of-tuple/template.ts
+++ type-challenges/questions/21220-medium-permutations-of-tuple/template.ts
@@ -1,1 +1,10 @@
-type PermutationsOfTuple<T extends unknown[]> = any
+type Insert<T extends unknown[], U> = T extends [infer F, ...infer L]
+  ? [F, U, ...L] | [F, ...Insert<L, U>]
+  : [U]
+
+type PermutationsOfTuple<
+  T extends unknown[],
+  Result extends unknown[] = []
+> = T extends [infer F, ...infer L]
+  ? PermutationsOfTuple<L, Insert<Result, F> | [F, ...Result]>
+  : Result
