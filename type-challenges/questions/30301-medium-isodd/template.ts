418a467e39919b315f5180b5e6425412af8e4732
BryanAdamss <bryanadamss@foxmail.com>
2023-11-18 21:45:55 +0800

    feat: 30301-medium-isodd

--- type-challenges/questions/30301-medium-isodd/template.ts
+++ type-challenges/questions/30301-medium-isodd/template.ts
@@ -1,1 +1,9 @@
-type IsOdd<T extends number> = any
+/** 思路:判断最后一位数是否特定值即可 */
+type IsOdd<T extends number> = `${T}` extends `${number | ''}${
+  | 1
+  | 3
+  | 5
+  | 7
+  | 9}`
+  ? true
+  : false
