56a0aeb25aa26aecd9f8877842810b7c7698f025
BryanAdamss <bryanadamss@foxmail.com>
2023-11-18 20:42:59 +0800

    feat: 21106-medium-zu-he-jian-lei-xing-combination-key-type

--- type-challenges/questions/21106-medium-zu-he-jian-lei-xing-combination-key-type/template.ts
+++ type-challenges/questions/21106-medium-zu-he-jian-lei-xing-combination-key-type/template.ts
@@ -1,2 +1,6 @@
-// 实现 Combs
-type Combs<T extends any[]> = any
+type Combs<T extends string[]> = T extends [
+  /** 知识点,infer 推断时可用 extends 约束为特定类型 */ infer F extends string,
+  ...infer R extends string[]
+]
+  ? `${F} ${R[number]}` | Combs<R>
+  : never
