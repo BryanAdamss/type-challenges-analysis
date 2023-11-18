684c9677d41dae15c8325b8ee0f174d780149331
BryanAdamss <bryanadamss@foxmail.com>
2023-11-18 21:40:16 +0800

    feat: 27958-medium-checkrepeatedtuple

--- type-challenges/questions/27958-medium-checkrepeatedtuple/template.ts
+++ type-challenges/questions/27958-medium-checkrepeatedtuple/template.ts
@@ -1,1 +1,8 @@
-type CheckRepeatedTuple<T extends unknown[]> = any
+type CheckRepeatedTuple<
+  T extends unknown[],
+  U extends unknown[] = []
+> = T extends [infer F, ...infer Rest]
+  ? Includes<U, F> extends true
+    ? true
+    : CheckRepeatedTuple<Rest, [...U, F]>
+  : false
